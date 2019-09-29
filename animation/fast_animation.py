eec5fc497c73aebef2712de0317b3547883daed2
Simon Thor <simtho@kth.se>
2019-09-29 18:05:26 +0200

    Added option to render video instead of saving + bug fixes

--- animation/fast_animation.py
+++ animation/fast_animation.py
@@ -8,17 +8,38 @@ import subprocess
 
 
 save_video = True
-fps = 60    # Frames per second
-color = 'b'
-dpi = 300   # Dots per inch (i.e. resolution)
-boxsize = [20, 20, 20]   # Length of each side of the box
-particlesize = 1            # Size of particles
+fps = 60                    # Frames per second
+color = 'b'                 # Color of particles
+dpi = 300                   # Dots per inch (i.e. resolution)
+boxsize = [100, 100, 100]      # Length of each side of the box
+particlesize = 1            # Size of particles (not the same coordinates as the box)
 filename = "test123.txt"    # Insert file name here (must be txt format)
-videoname = "test_animation.mp4"    # Name of the rendered video. Must include file format
-multiprocessing = True
+videoname = "test_animation"    # Name of the video. Without file format
+if save_video:
+    multiprocessing = True  # Enable this for parallell processing
+    if multiprocessing:
+        cpus = 4            # number of cores in computer
+else:
+    multiprocessing = False
+
+
+def test_datagen(nparticles, nframes, filename):
+    """Function that generates random data and saves in a file. Used for testing the program."""
+    a = np.random.uniform(0, 10, size=(nparticles, 3))
+    with open(filename, 'w') as txt:
+        for i in range(nframes):
+            a += 0.1
+            for col in a:
+                for i in col:
+                    txt.write(str(i) + " ")
+                txt.write("\t")
+            txt.write("\n")
 
 
 def data_extract(filename):
+    """Function that extracts data from the file that stores data.
+    Returns: numpy array with location of all particles for each frame"""
+
     simulation = []
     with open(filename) as txtdata:
         for frame in txtdata.readlines():
@@ -36,6 +57,10 @@ def data_extract(filename):
 
 
 def animate(data, index):
+    """Function that animates all particles in 3D.
+    NOTE: This function is rather slow. Improvements might be possible.
+    Returns: None but creates an mp4 file with the animation."""
+
     frames = []
     fig = plt.figure()
     ax = plt.axes(projection='3d')
@@ -54,20 +79,27 @@ def animate(data, index):
     fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
     fig.set_size_inches((5, 5), forward=True)
 
-    writer = animation.FFMpegWriter(fps=fps, bitrate=fps*100, extra_args=['-vcodec', 'libx264', '-preset', 'ultrafast'])
+    if save_video:
+        writer = animation.FFMpegWriter(fps=fps, bitrate=fps*100, extra_args=['-vcodec', 'libx264', '-preset', 'ultrafast'])
+        tbefore = time.time()
+        plot = ax.plot([], [], [], '.', ms=particlesize, c=color)[0]
 
-    plot = ax.plot([], [], [], '.', ms=particlesize, c=color)[0]
-
-    tbefore = time.time()
-    with writer.saving(fig, f"../videos/{index if index > 0 else ""}{videoname}", dpi=dpi):
+        with writer.saving(fig, f"../videos/{index if index >= 0 else ''}{videoname}.mp4", dpi=dpi):
+            for frame in data:
+                plot.set_data(frame[:, 0], frame[:, 1])
+                plot.set_3d_properties(frame[:, 2])
+                # grab_frame takes time
+                writer.grab_frame()
+        tafter = time.time()
+        print("time taken to save video:", tafter - tbefore, "s")
+    else:
+        video = []
         for frame in data:
-            plot.set_data(frame[:, 0], frame[:, 1])
-            plot.set_3d_properties(frame[:, 2])
-            #writer._frame_sink().write(writer.fig.canvas.tostring_argb())
-            # This is the part that takes time
-            writer.grab_frame()
-    tafter = time.time()
-    print("time taken to render video:", tafter - tbefore, "s")
+            plot = ax.scatter(frame[:, 0], frame[:, 1], frame[:, 2], s=particlesize, c=color, animated=True)
+            video.append([plot])
+        # TODO: fix frame rate
+        animation.ArtistAnimation(fig, video, interval=1000/fps, blit=True, repeat_delay=1000)
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -75,12 +107,11 @@ if __name__ == "__main__":
     data = data_extract(filename)
 
     print("animating...")
-    if multiprocessing:
-        cpus = 4
+    if multiprocessing and save_video:
         tf = [int(i/cpus*data.shape[0]) for i in range(cpus+1)]
         print(tf)
         with Pool(cpus) as p:
             p.starmap(animate, [(data[tf[i]:tf[i+1],:,:], i) for i in range(len(tf)-1)])
-        subprocess.call("./merge_videos.sh")
+        subprocess.call(["./merge_videos.sh", videoname])
     else:
         animate(data, -1)
--- animation/merge_videos.sh
+++ animation/merge_videos.sh
@@ -4,18 +4,19 @@ string=""
 cpus=4
 for ((i=0;i<cpus;i++))
 do
-  filename="../videos/${i}test_animation"
+  filename="../videos/${i}$1"
   ffmpeg -i "${filename}.mp4" -c copy -bsf:v h264_mp4toannexb -f mpegts -loglevel fatal "${filename}.ts"
   string="${string}|${filename}.ts"
 done
 string=${string:1:${#string}-1}
 
-ffmpeg -i "concat:${string}" -c copy -loglevel fatal -bsf:a aac_adtstoasc "../videos/output.mp4"
+ffmpeg -i "concat:${string}" -c copy -loglevel fatal -bsf:a aac_adtstoasc "../videos/$1.mp4"
 
 for ((i=0;i<cpus;i++))
 do
-  filename="../videos/${i}test_animation"
+  filename="../videos/${i}$1"
   rm "${filename}.ts"
+  rm "${filename}.mp4"
 done
 
 # string=""
